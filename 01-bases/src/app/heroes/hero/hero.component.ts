1c2ffe39749d9dd7a01bb6999e02d3ff0e9e3212
thedraxx <thedraxxgames@gmail.com>
2023-10-31 16:29:59 -0300

    directiva ngfor

--- 01-bases/src/app/heroes/hero/hero.component.ts
+++ 01-bases/src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  public get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  public getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  public changeHero(): void {
+    this.name = 'spiderman';
+  }
+
+  public changeAge(): void {
+    this.age = 50;
+  }
+
 }
